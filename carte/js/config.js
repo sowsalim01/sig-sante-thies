9029c6a1470196b7b461adb00e103f46918854f4
sowsalim01 <sowsalim01@gmail.com>
2025-01-28 08:14:32 +0000

    changement

--- carte/js/config.js
+++ carte/js/config.js
@@ -1,3 +1,5 @@
 // config.js
 const SUPABASE_URL = 'https://dbwxeqvavkhoqjrirkbh.supabase.co'; // Ton URL Supabase
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRid3hlcXZhdmtob3Fqcmlya2JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM2NjEyNTcsImV4cCI6MjA0OTIzNzI1N30.oWjy68DareHBqVmT6Q7CBWlr90rg8vmxorDz2KAZsCU'; // Ta clé publique (API Key)
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
